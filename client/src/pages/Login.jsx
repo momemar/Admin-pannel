955e9a01fc4e98a212752425734029482dd8cc2c
“momemar” <mmemar@logisx.net>
2023-07-17 15:45:02 -0500

    adjust some styling with bootstrape

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Login = (props) => {
   const [email, setEmail] = useState('');
@@ -13,44 +15,53 @@ const Login = (props) => {
   };
 
   return (
-    <div className='auth-form-container'>
-      <form className='login-form' onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="youremail@gmail.com"
-          id="email"
-          name="email"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="*********"
-          id="password"
-          name="password"
-        />
-        <div className="remember-me-container">
-          <input
-            type="checkbox"
-            checked={rememberMe}
-            onChange={(e) => setRememberMe(e.target.checked)}
-            id="rememberMe"
-            name="rememberMe"
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}>
+      <Container>
+        <Form className="auth-form-container login-form" onSubmit={handleSubmit}>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="youremail@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <label htmlFor="rememberMe">Remember Me</label>
-        </div>
-        <button className='link-btn' type="submit">Login</button>
-        <div className="forgot-password">
-          {/* <a href="#">Forgot Password?</a> */}
-        </div>
-      </form>
-      <button onClick={() => props.onFormSwitch('Signup')}>
-        Don't have an account? Register here
-      </button>
+
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="*********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <Form.Check >
+            <Form.Check.Input
+              type="checkbox"
+              checked={rememberMe}
+              onChange={(e) => setRememberMe(e.target.checked)}
+              id="rememberMe"
+              name="rememberMe"
+            />
+            <Form.Check.Label className="remember-me-container" htmlFor="rememberMe" style={{ paddingTop: '3px' }}>Remember Me</Form.Check.Label>
+          </Form.Check>
+
+          <Button className="link-btn" type="submit" variant="success">
+            Login
+          </Button>
+
+          <div className="forgot-password" style={{ fontSize: '8px', textAlign: 'left' }}>
+            <a href="#">Forgot Password?</a>
+          </div>
+          <Button onClick={() => props.onFormSwitch('Signup')} variant="blank" style={{ fontSize: '8px', textAlign: 'right' }}>
+            Create new account
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 };

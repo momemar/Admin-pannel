6a3ec8c2c09fa96dd431aa405a2b412fd3464d33
“momemar” <mmemar@logisx.net>
2023-07-17 13:20:35 -0500

    added some css also added forgot password box

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,30 +3,56 @@ import React, { useState } from 'react';
 const Login = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
 
-
-  const hanleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('email: ', email);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Remember Me:', rememberMe);
   };
 
-
-
   return (
-    <>
-      <form onSubmit={hanleSubmit} >
+    <div className='auth-form-container'>
+      <form className='login-form' onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+        />
         <label htmlFor="password">Password</label>
-        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*********" id="password" name="password" />
-        <button type="submit">Login</button>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="*********"
+          id="password"
+          name="password"
+        />
+        <div className="remember-me-container">
+          <input
+            type="checkbox"
+            checked={rememberMe}
+            onChange={(e) => setRememberMe(e.target.checked)}
+            id="rememberMe"
+            name="rememberMe"
+          />
+          <label htmlFor="rememberMe">Remember Me</label>
+        </div>
+        <button className='link-btn' type="submit">Login</button>
+        <div className="forgot-password">
+          {/* <a href="#">Forgot Password?</a> */}
+        </div>
       </form>
-      <button onClick={() => props.onFormSwitch('Signup')}>Don't have an account? Register here</button>
-
-    </>
-
+      <button onClick={() => props.onFormSwitch('Signup')}>
+        Don't have an account? Register here
+      </button>
+    </div>
   );
 };
 
 export default Login;
-
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -10,19 +10,19 @@ const Signup = (props) => {
   };
 
   return (
-    <>
-      <form onSubmit={hanleSubmit} >
+    < div className='auth-form-container' >
+      <form className='signup-form' onSubmit={hanleSubmit} >
         <label htmlFor='name' >Full Name</label>
         <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="full name" id="name" name="name" />
         <label htmlFor="email">Email</label>
         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
         <label htmlFor="password">Password</label>
         <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*********" id="password" name="password" />
-        <button type="submit">Login</button>
+        <button className='link-btn' type="submit">Login</button>
       </form>
       <button onClick={() => props.onFormSwitch('login')}>Already have an account? Login here</button>
 
-    </>
+    </div >
   );
 };
 
